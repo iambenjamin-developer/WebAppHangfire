9a5e5eb825eea8bfb5b05c3dad6397e36063a3fa
Benjamin Correa <benjamin.c@smartwaystudio.com>
2022-07-05 12:05:37 -0300

    Basic Tests Jobs

---
+++ WebAppHangfire/Controllers/TestsController.cs
@@ -0,0 +1,43 @@
+﻿using Hangfire;
+using Microsoft.AspNetCore.Mvc;
+
+namespace WebAppHangfire.Controllers
+{
+    [Route("[controller]")]
+    [ApiController]
+    public class TestsController : ControllerBase
+    {
+
+        [HttpGet("Fire-and-Forget-Jobs")]
+        public void Enqueue()
+        {
+            var jobId = BackgroundJob.Enqueue(() =>
+                          Console.WriteLine("Fire-and-forget!"));
+        }
+
+        [HttpGet("Delayed-Jobs")]
+        public void Schedule()
+        {
+            var jobId = BackgroundJob.Schedule(() =>
+                          Console.WriteLine("Delayed!"), TimeSpan.FromMinutes(3));
+        }
+
+        [HttpGet("Recurring-Jobs")]
+        public void AddOrUpdate()
+        {
+            RecurringJob.AddOrUpdate("myrecurringjob", () =>
+                    Console.WriteLine("Recurring!"), Cron.Minutely);
+        }
+
+        [HttpGet("Continuations")]
+        public void ContinueJobWith()
+        {
+            var jobId = BackgroundJob.Enqueue(() =>
+            Console.WriteLine("First Job!"));
+
+
+            BackgroundJob.ContinueJobWith(jobId, () =>
+                      Console.WriteLine("Continuation!"));
+        }
+    }
+}
